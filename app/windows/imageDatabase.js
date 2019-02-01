b579f8487c6e7136cbf836d42666c17f7dfcb41f
Brett Doyle <brettd3000@gmail.com>
2019-02-01 13:30:13 +0000

    Fixed issues with image library, added remove button

--- app/windows/imageDatabase.js
+++ app/windows/imageDatabase.js
@@ -25,18 +25,22 @@ const imageAddButton = new layout.FileInput({
         }
     ]
 })
-const imageList = new layout.List({
-    reoderable: false,
-    editButton: false,
-    removeButton: true,
+const imageList = new layout.TableList({
+    columns: 2,
+    columnWidths: ['', '5ch']
+})
 
-    addInput: false
+const removeAllButton = new layout.Button({
+    text: 'Remove All'
 })
 
 const imageTitle = new layout.Text(
     {},
     {
-        overflow: 'ellipsis'
+        overflow: 'ellipsis',
+        grow: 1,
+        align: 'center',
+        textAlign: 'left'
     }
 )
 const imagePreview = new layout.Image(
@@ -55,6 +59,10 @@ const imagePreview = new layout.Image(
     }
 )
 
+const removeButton = new layout.Button({
+    text: 'Remove'
+})
+
 layout.body.add(
     new layout.LayoutBlock({
         items: [
@@ -62,7 +70,7 @@ layout.body.add(
                 items: [
                     new layout.Block(
                         {
-                            items: [imageAddButton, imageList],
+                            items: [imageAddButton, imageList, removeAllButton],
                             childSpacing: '8px'
                         },
                         {
@@ -80,7 +88,22 @@ layout.body.add(
                 items: [
                     new layout.Block(
                         {
-                            items: [imageTitle, imagePreview],
+                            items: [
+                                new layout.Block(
+                                    {
+                                        items: [imageTitle, removeButton],
+                                        childSpacing: '8px'
+                                    },
+                                    {
+                                        padding: '',
+                                        direction: 'horizontal',
+
+                                        grow: 0,
+                                        shrink: 0
+                                    }
+                                ),
+                                imagePreview
+                            ],
                             childSpacing: '8px'
                         },
                         {
@@ -100,66 +123,85 @@ layout.body.add(
     })
 )
 
-function updateImageList(toSelect) {
-    if (typeof toSelect !== 'string') {
-        toSelect = imageList.selected
-    }
+let updatingImages = false
+
+function toggleInterface(enabled) {
+    imageAddButton.disabled = !enabled
+    removeAllButton.disabled = !enabled
+    removeButton.disabled = !enabled
+}
+
+function updateImageList() {
+    let toSelect = imageList.selected
 
     imageList.clear()
 
+    if (Images.files.length === 0) {
+        removeAllButton.disabled = true
+
+        showImage()
+
+        return false
+    }
+    removeAllButton.disabled = false
+
     Images.files.forEach(image => {
-        imageList.add(image)
+        imageList.add([
+            path.basename(image, path.extname(image)),
+            path.extname(image)
+        ])
     })
 
-    if (imageList.indexOf(toSelect) !== -1) {
-        imageList.select(toSelect, false, true)
-    } else {
-        imageList.select(0, false, true)
+    imageList.select(toSelect, true)
+
+    if (imageList.selectedIndex === -1) {
+        imageList.select(0, true)
     }
 }
 
-imageList.onEvent('select', event => {
-    let image = Images.files.find(image => image === event.text)
-
+function showImage(image) {
     if (!image) {
+        imageTitle.text = ''
+        imagePreview.url = ''
+
+        removeButton.disabled = true
+
         return false
     }
 
     imageTitle.text = image
     imagePreview.url = path.join(Images.directory, image)
-})
 
-imageList.onEvent('remove', event => {
-    if (!event.fromUser) {
-        return false
-    }
+    removeButton.disabled = false
+}
 
-    if (!Images.files.includes(event.text)) {
-        return false
-    }
-
-    Images.remove(event.text, error => {
-        if (error) {
-            layout.dialog.showError({
-                message: 'Unable to delete image',
-                detail: error.message || error.toString()
-            })
-
-            logger.error("Couldn't remove image from database", error)
-        }
-
-        if (Images.files.length === 0) {
-            imagePreview.url = ''
-            imageTitle.text = ''
-        }
-    })
+imageList.onEvent('select', event => {
+    showImage(
+        Images.files.find(image => {
+            return (
+                path.basename(image, path.extname(image)) === event.text[0] &&
+                path.extname(image) === event.text[1]
+            )
+        })
+    )
 })
 
 imageAddButton.onEvent('open', event => {
     let toAdd = event.files
 
+    updatingImages = true
+    layout.showLoader(imageList, 'Adding')
+    toggleInterface(false)
+
     let addNext = () => {
         if (toAdd.length === 0) {
+            toggleInterface(true)
+
+            updateImageList()
+            updatingImages = false
+
+            layout.hideLoader(imageList)
+
             return false
         }
 
@@ -177,7 +219,10 @@ imageAddButton.onEvent('open', event => {
                 logger.error("Couldn't save image to database!", error)
             }
 
-            imageList.select(uniqueFileName)
+            imageList.select([
+                path.basename(uniqueFileName, path.extname(uniqueFileName)),
+                path.extname(uniqueFileName)
+            ])
 
             addNext()
         })
@@ -186,14 +231,99 @@ imageAddButton.onEvent('open', event => {
     addNext()
 })
 
+removeButton.onEvent('click', () => {
+    let selectedImage = imageList.selected
+    if (!selectedImage) {
+        return false
+    }
+
+    let image = Images.files.find(image => {
+        return (
+            path.basename(image, path.extname(image)) === selectedImage[0] &&
+            path.extname(image) === selectedImage[1]
+        )
+    })
+
+    if (!image) {
+        return false
+    }
+
+    Images.remove(image, error => {
+        if (error) {
+            if (error) {
+                layout.dialog.showError({
+                    message: 'Unable to delete image',
+                    detail: error.message || error.toString()
+                })
+
+                logger.error("Couldn't remove image from database", error)
+            }
+        }
+
+        updateImageList()
+    })
+})
+
+removeAllButton.onEvent('click', event => {
+    layout.dialog.showQuestion(
+        {
+            title: 'Remove all images?',
+
+            message: 'Are you sure you want to remove all images?',
+            detail: 'This action cannot be undone!',
+
+            options: ['Remove', 'Cancel']
+        },
+        (error, answer) => {
+            if (answer === 'Remove') {
+                updatingImages = true
+                layout.showLoader(imageList, 'Removing')
+                toggleInterface(false)
+
+                let removeNext = error => {
+                    if (error) {
+                        if (error) {
+                            layout.dialog.showError({
+                                message: 'Unable to delete image',
+                                detail: error.message || error.toString()
+                            })
+
+                            logger.error(
+                                "Couldn't remove image from database",
+                                error
+                            )
+                        }
+                    }
+
+                    if (Images.files.length === 0) {
+                        toggleInterface(true)
+
+                        updateImageList()
+                        updatingImages = false
+
+                        layout.hideLoader(imageList)
+                    } else {
+                        Images.remove(Images.files[0], removeNext)
+                    }
+                }
+
+                removeNext()
+            }
+        }
+    )
+})
+
 Images.onEvent('update-start', () => {
     layout.showLoader(imageList)
 })
 
 Images.onEvent('update', () => {
-    layout.hideLoader(imageList)
+    if (updatingImages) {
+        return false
+    }
 
     updateImageList()
+    layout.hideLoader(imageList)
 })
 
 Images.onEvent('error', error => {
