ad24cc3ff04190f878354dcafc8c1f10cb0456ff
Brett Doyle <brettd3000@gmail.com>
2019-02-05 13:21:45 +0000

    Simplified scroll update animation frames

--- app/windows/control.js
+++ app/windows/control.js
@@ -614,49 +614,45 @@ const playlist = {}
 
     let scrollPosition = { index: 0, subIndex: 0 }
 
-    function doScroll() {
-            if (
-                options.autoScroll === false ||
-                scrollPosition.index < 0 ||
-                scrollPosition.index >= itemsBlock.items.length ||
-                scrollPosition.subIndex < 0 ||
-                scrollPosition.subIndex >=
-                    itemsBlock.items[scrollPosition.index].items.length
-            ) {
-                return false
-            }
+    function updateScroll() {
+        if (
+            options.autoScroll === false ||
+            scrollPosition.index < 0 ||
+            scrollPosition.index >= itemsBlock.items.length ||
+            scrollPosition.subIndex < 0 ||
+            scrollPosition.subIndex >=
+                itemsBlock.items[scrollPosition.index].items.length
+        ) {
+            return false
+        }
 
-            let node =
-                itemsBlock.items[scrollPosition.index].items[
-                    scrollPosition.subIndex
-                ].node
+        let node =
+            itemsBlock.items[scrollPosition.index].items[
+                scrollPosition.subIndex
+            ].node
 
-            let listHeight = itemsBlock.node.offsetHeight
-            let listScroll = itemsBlock.node.scrollTop
+        let listHeight = itemsBlock.node.offsetHeight
+        let listScroll = itemsBlock.node.scrollTop
 
-            let itemCenter =
-                node.offsetTop +
-                node.offsetHeight / 2 -
-                itemsBlock.node.offsetTop
+        let itemCenter =
+            node.offsetTop + node.offsetHeight / 2 - itemsBlock.node.offsetTop
 
-            let listPadding = Math.min(node.offsetHeight * 2.6, listHeight / 2)
+        let listPadding = Math.min(node.offsetHeight * 2.6, listHeight / 2)
 
-            if (itemCenter - listPadding < listScroll) {
-                itemsBlock.node.scrollTo({
-                    top: itemCenter - listPadding,
-                    left: 0,
-                    behavior: options.scrollSmooth ? 'smooth' : 'auto'
-                })
-            } else if (itemCenter + listPadding > listScroll + listHeight) {
-                itemsBlock.node.scrollTo({
-                    top: itemCenter - (listHeight - listPadding),
-                    left: 0,
-                    behavior: options.scrollSmooth ? 'smooth' : 'auto'
-                })
-            }
+        if (itemCenter - listPadding < listScroll) {
+            itemsBlock.node.scrollTo({
+                top: itemCenter - listPadding,
+                left: 0,
+                behavior: options.scrollSmooth ? 'smooth' : 'auto'
+            })
+        } else if (itemCenter + listPadding > listScroll + listHeight) {
+            itemsBlock.node.scrollTo({
+                top: itemCenter - (listHeight - listPadding),
+                left: 0,
+                behavior: options.scrollSmooth ? 'smooth' : 'auto'
+            })
+        }
     }
-
-    let scrollNextFrame = () => {layout.onFrame.start(doScroll)}
 
     function scrollTo(position) {
         if (
@@ -672,7 +668,10 @@ const playlist = {}
         scrollPosition.index = position.index
         scrollPosition.subIndex = position.subIndex
 
-        layout.onFrame.start(scrollNextFrame)
+        //The scrollTo function waits for the end of an animation frame to actually update
+        //This is because items may get maximized/minimized when going through the playlist
+        //And their height only updates on an animation frame, so by waiting for the end of one, all items should be at the correct height when scrolling
+        layout.onFrame.end(updateScroll)
     }
 
     //Updates previews and sends display message
