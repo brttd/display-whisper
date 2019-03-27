885a6d2c10867571105fd540f7469cd6307a8768
Brett Doyle <brettd3000@gmail.com>
2019-03-27 11:31:12 +0000

    Added process-icons script

---
+++ scripts/process-icons.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+function modifyIconFile(fileName) {
+    fs.readFile(fileName, 'utf8', (error, content) => {
+        if (error) {
+            console.error(filename, error)
+            return false
+        }
+
+        content = content.replace(
+            /<[\W\w]*<svg.*>/gim,
+            '<svg xmlns="http://www.w3.org/2000/svg">'
+        )
+        content = content.replace(/<g/gim, '<symbol viewbox="0 0 18 18"')
+        content = content.replace(/<\/g/gim, '</symbol')
+
+        content = content.replace(/fill:white;/gim, '')
+        content = content.replace(/[\s]*style=""/gim, '')
+
+        fs.writeFile(fileName, content, error => {
+            if (error) {
+                console.error(filename, error)
+            }
+        })
+    })
+}
+
+fs.readdir('app/icons', (error, list) => {
+    if (error) {
+        console.error(error)
+        return false
+    }
+
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].includes('.svg')) {
+            modifyIconFile('app/icons/' + list[i])
+        }
+    }
+})
