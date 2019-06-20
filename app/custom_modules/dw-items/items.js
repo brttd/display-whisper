8b1af1df34055b8e92d8ea006d3f52f6b46d163b
Brett Doyle <brettd3000@gmail.com>
2019-06-20 15:22:29 +0100

    Added pdf item type

--- app/custom_modules/dw-items/items.js
+++ app/custom_modules/dw-items/items.js
@@ -3,5 +3,6 @@ const path = require('path')
 exports['image'] = require(path.join(__dirname, '../../items/image'))
 exports['song'] = require(path.join(__dirname, '../../items/song'))
 exports['text'] = require(path.join(__dirname, '../../items/text'))
+exports['pdf'] = require(path.join(__dirname, '../../items/pdf'))
 
-exports.list = ['image', 'song', 'text']
+exports.list = ['image', 'song', 'text', 'pdf']
